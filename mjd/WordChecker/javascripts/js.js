d86c831041ef2fb54939ff1876092a0560c2aac4
williamhickok11 <williamhickok11@gmail.com>
2016-02-22 19:05:42 -0600

    finished word checker and cal

--- mjd/WordChecker/javascripts/js.js
+++ mjd/WordChecker/javascripts/js.js
@@ -1,12 +1,57 @@
-// Now that the test suite exists, write a simple application to implement the functionality.
+"use strict";
 
-// In an HTML file, have two elements: a textarea and a button.
-// When the button is clicked, extract the contents of the textarea and validate it against the following rules.
-// There should only be alphanumeric characters in the text. No special characters like $%^&*.
-// No duplicate words allowed.
-// Word count should be less than 100.
+// There should be a function defined with the name checkWordCount.
+// That function should return false if a string sent to it has more than 100 words in it.
+
+// There should be a function defined with the name duplicateCheck.
+// That function should return false if a string has duplicate words in it.
+
+// There should be a function defined with the name verifyAlphaNumeric.
+// That function should return false if there are any non-alphanumeric characters in a string.
+
+var checkWordCount = function (userString) {
+  var userStringArray = userString.split(" ");
+  if (userStringArray.length > 100) {
+    console.log("false");
+    return false;
+  } else {
+    console.log("less than 100");
+  };
+};
+
+var duplicateCheck = function (userString) {
+  var userStringArray = userString.split(" ");
+  for (var i = 0; i < userStringArray.length; i++) {
+    for (var j = 0; j < userStringArray.length; j++) {
+      if (userStringArray[i] === userStringArray[j]) {
+        if (i !== j) {
+          console.log("False: There is a repeated word");
+          return false;
+        };
+      };
+    };
+  };
+};
+
+var verifyAlphaNumeric = function (userString) {
+  var userStringArray = userString.split(" ");
+  for (var i = 0; i < userStringArray.length; i++) {
+    userStringArray[i]
+    if(/^[a-zA-Z0-9- ]*$/.test(userStringArray[i]) == false) {
+      console.log('Your string contains illegal characters.');
+      return false;
+    };
+  };
+};
+
 
 $("#aButton").on("click", function(){
   var currentString = $("#userStringInput").val();
-  console.log("currentString", currentString);
-})
+
+  // Word count should be less than 100.
+  checkWordCount(currentString);
+  // No duplicate words allowed.
+  duplicateCheck(currentString);
+  // There should only be alphanumeric characters in the text. No special characters like $%^&*.
+  verifyAlphaNumeric(currentString);
+});
